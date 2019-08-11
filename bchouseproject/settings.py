8310497de8b283bb816cde41440fa42fca3476be
Young Lee <jaeyounglee327@gmail.com>
2019-08-11 00:13:15 -0400

    allowed hosts

--- bchouseproject/settings.py
+++ bchouseproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bchouse-ca.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
