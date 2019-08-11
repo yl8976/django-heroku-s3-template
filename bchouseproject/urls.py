e8fcf68a4c60d5cf30cf352d7c6a83a1e949b9d6
Young Lee <jaeyounglee327@gmail.com>
2019-08-11 00:26:54 -0400

    static

--- bchouseproject/urls.py
+++ bchouseproject/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     url(r"^admin/", admin.site.urls),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
